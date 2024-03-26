8697f8885af60c82391d7c9b10dac424334d4a3c
Nicholas <mickyaw1995@gmail.com>
2024-03-26 14:21:32 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -25,7 +25,8 @@ function createApp(database: Database) {
     res.json({ cost });
   });
   function parsePlainDate(dateString: string | undefined){
-    if(dateString) return null
+    if(dateString)
+     return null
   }
   function parseDate(dateString: string | undefined): Date | undefined {
     if (dateString) {
