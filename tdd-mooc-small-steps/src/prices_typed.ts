494382c3a603bc8fbf3396a1b41e9a1b069f78aa
Nicholas <mickyaw1995@gmail.com>
2024-03-26 15:16:34 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -93,7 +93,7 @@ function createApp(database: Database) {
       if (
         date &&
         date2?.equals(holiday2) &&
-        date.getFullYear() === holiday.getFullYear() &&
+  
    
         date.getDate() === holiday.getDate()
       ) {
