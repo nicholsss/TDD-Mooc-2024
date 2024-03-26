ef43e5852f7ad44e59bc12c40dd112f767edf602
Nicholas <mickyaw1995@gmail.com>
2024-03-26 15:14:56 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -92,6 +92,7 @@ function createApp(database: Database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
         date &&
+        date2?.equals(holiday2) &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
