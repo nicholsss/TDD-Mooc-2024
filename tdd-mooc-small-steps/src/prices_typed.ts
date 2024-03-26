7c2b62dde8d47502741a47da954182d2d5b754b8
Nicholas <mickyaw1995@gmail.com>
2024-03-26 15:18:08 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices_typed.ts
+++ tdd-mooc-small-steps/src/prices_typed.ts
@@ -91,8 +91,7 @@ function createApp(database: Database) {
       let holiday = new Date(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
-        date && date2?.equals(holiday2) 
-      ) {
+        date && date2?.equals(holiday2) ) {
         return true;
       }
     }
